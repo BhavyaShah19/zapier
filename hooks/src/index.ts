915adffa59ab3addb3ea932dc35ecb4887d07335
Bhavya Shah <bshah19112001@gmail.com>
2025-06-23 13:27:23 +0530

    added schema and transaction

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,7 +1,30 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
+const client = new PrismaClient();
 const app = express();
+app.use(express.json());
 
-app.post('/hooks/catch/:userId/:zapId', (req, res) => {
-  const { userId, zapId } = req.params;
+app.post('/hooks/catch/:userId/:zapId', async (req, res) => {
+    const { userId, zapId } = req.params;
+    const { body } = req.body
+
+    await client.$transaction(async (tx) => {
+        const run = await tx.zapRun.create({
+            data: { 
+                zapId: zapId,
+                metadata: body,
+            },
+        });
+        await tx.zapRunOutbox.create({
+            data: {
+                zapRunId: run.id,
+            }
+        })
+    });
+    res.json({message:"Webhook received"});
 });
+
+app.listen(3000, () => {
+    console.log('listening on port 3000');  
+})
